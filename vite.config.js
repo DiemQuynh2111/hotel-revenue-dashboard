b7d31489069e417d4ce5a0904cbb1e5f1ed8c950
DiemQuynh <nguyenthidiemquynh2011@gmail.com>
2026-04-30 23:02:45 +0700

    update base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === "build" ? "/hotel-revenue-dashboard/" : "/",
-}));
+  // Quan trọng: Phải để dấu gạch chéo ở đầu và cuối như thế này
+  base: "/hotel-revenue-dashboard_DEXUAT/", 
+});
