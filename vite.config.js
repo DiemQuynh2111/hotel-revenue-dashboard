d5980d8bdb3ef1a70c78b4d12de8be49f2062f9b
DiemQuynh <nguyenthidiemquynh2011@gmail.com>
2026-04-30 23:39:20 +0700

    Fix code before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // Quan trọng: Phải để dấu gạch chéo ở đầu và cuối như thế này
-  base: "/hotel-revenue-dashboard_DEXUAT/", 
-});
+  base: command === "build" ? "/hotel-revenue-dashboard/" : "/",
+}));
